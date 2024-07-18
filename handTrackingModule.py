d6b54194cc944bf9c4c0ffdc2364af679c3ba3dd
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-07-18 20:42:22 +0630

    first commit

---
+++ handTrackingModule.py
@@ -0,0 +1,48 @@
+import cv2  # Import OpenCV library
+import mediapipe as mp  # Import MediaPipe library
+import time  # Import time library
+
+# Initialize video capture object to capture from the default camera
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+# Infinite loop to continuously capture and process video frames
+while True:
+    # Read a frame from the video capture object
+    success, img = cap.read()
+    
+    img = cv2.flip(img, 1)
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255,0,255), cv2.FILLED)
+            
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    
+    # Display the captured frame in a window titled "Image"
+    cv2.imshow("Image", img)
+    
+    # Wait for 1 millisecond before processing the next frame
+    cv2.waitKey(1)
